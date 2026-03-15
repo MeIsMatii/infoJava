6eac51f51238c4f2b3910c3cc74a9f737c878979
matii <eichbauer.mathilo@gmail.com>
2026-03-15 01:36:21 +0100

    moew

--- projects/VideoPlayer/src/Level1.java
+++ projects/VideoPlayer/src/Level1.java
@@ -27,11 +27,22 @@ public class Level1 extends World
     static int HEIGHT = 720;
     */
 
+    //image
+    /*
+    private static final int FRAME_AMOUNT = 1;
+    private static final String PATH = "generate/"; // u gotta change this too
+    static int WIDTH = 1280;
+    static int HEIGHT =1000;
+    static String FILENAME = "febfeb";
+    */
+
 
     private static final GreenfootImage[] frames = new GreenfootImage[FRAME_AMOUNT];
-    private static final int CELL_SIZE = 1;
+    private static final int CELL_SIZE = 2;
     public Level1(){
         super(WIDTH, HEIGHT, CELL_SIZE);
+        //video
+
         for (int i = 0; i < frames.length; i++) {
             if(i+1<10) {
                 frames[i] = new GreenfootImage(PATH + "out_00" + (i +1) + ".png");
@@ -40,8 +51,10 @@ public class Level1 extends World
             } else {
                 frames[i] = new GreenfootImage(PATH + "out_" + (i+1) + ".png");
             }
-
         }
+
+        //image
+        //frames[0] = new GreenfootImage(PATH + FILENAME + ".png");
         setPaintOrder(Pixel.class, ImageLoader.class);
         //ImageLoader image = new ImageLoader(gImg);
         VideoLoader video = new VideoLoader(frames,1);
@@ -52,13 +65,6 @@ public class Level1 extends World
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
-    private void prepare()
-    {
-
-    }
-
-    public void act(){
-
-    }
+   
 
 }
